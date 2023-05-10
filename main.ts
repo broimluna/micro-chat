d75cc5e22e079b3dfbe3dbd12d9b646a78ee7441
Luna <36826729+broimluna@users.noreply.github.com>
2023-05-10 14:37:47 -0400

    micro:chat v1.2

--- main.ts
+++ main.ts
@@ -11,7 +11,22 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P0, 0)
     basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # . # . .
+        # # # . .
+        # . # . .
+        # # # # #
+        `)
+})
+input.onPinPressed(TouchPin.P2, function () {
     basic.clearScreen()
+    for (let index = 0; index < 5; index++) {
+        basic.showIcon(IconNames.Target)
+        basic.pause(500)
+        basic.showIcon(IconNames.Diamond)
+        basic.pause(500)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     radio.setGroup(1)
@@ -39,7 +54,13 @@ input.onButtonPressed(Button.AB, function () {
         `)
     basic.pause(2000)
     pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        # . # . .
+        # # # . .
+        # . # . .
+        # # # # #
+        `)
 })
 radio.onReceivedString(function (receivedString) {
     for (let index = 0; index < 4; index++) {
@@ -57,15 +78,34 @@ radio.onReceivedString(function (receivedString) {
         basic.pause(500)
     }
     basic.showString(receivedString)
+    basic.showLeds(`
+        # # # # #
+        # . # . .
+        # # # . .
+        # . # . .
+        # # # # #
+        `)
 })
-input.onPinPressed(TouchPin.P1, function () {
-    basic.clearScreen()
-    for (let index = 0; index < 5; index++) {
-        basic.showIcon(IconNames.Target)
-        basic.pause(500)
-        basic.showIcon(IconNames.Diamond)
-        basic.pause(500)
-    }
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("micro:chat:b")
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # . # . .
+        # # # . .
+        # . # . .
+        # # # # #
+        `)
 })
 radio.setGroup(1)
 basic.showLeds(`
